6baebcacd90c53da7f33b91b0c3d585fea4fa67e
LaraFrenkel <frenkel.lara@comunidad.ub.edu.ar>
2026-09-14 10:10:26 -0300

    feat: add Prisma database foundation

---
+++ backend/src/config/database.ts
@@ -0,0 +1,12 @@
+import { PrismaPg } from "@prisma/adapter-pg";
+
+import { PrismaClient } from "../generated/prisma/client.js";
+import { environment } from "./environment.js";
+
+const adapter = new PrismaPg({ connectionString: environment.DATABASE_URL });
+
+export const database = new PrismaClient({ adapter });
+
+export async function disconnectDatabase(): Promise<void> {
+  await database.$disconnect();
+}
